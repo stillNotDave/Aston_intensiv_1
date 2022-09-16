a2a47961568cf3818b2ee51c007dbfccf338807d
stillNotDave <ujenevfqgjg@mail.ru>
2022-09-16 13:09:19 +0300

    master commit 2

--- app/src/main/java/ru/sfedu/gitcheck/MainActivity.java
+++ app/src/main/java/ru/sfedu/gitcheck/MainActivity.java
@@ -3,8 +3,13 @@ package ru.sfedu.gitcheck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView TV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
